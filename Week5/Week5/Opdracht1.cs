cc90aaf8927af7e44665aa1cd038b1af5605d8e5
Sven Mark Hofstra <hofs1600.student.nhl.nl>
2016-10-04 12:16:01 +0200

    opgave1week5fix

--- Week5/Week5/Opdracht1.cs
+++ Week5/Week5/Opdracht1.cs
@@ -13,9 +13,10 @@ namespace Week5
             int[] getallen;
             getallen = new int[3];
             Console.WriteLine("3 getallen aub: ");
-            getallen[0] = Convert.ToInt32(Console.ReadLine());
-            getallen[1] = Convert.ToInt32(Console.ReadLine());
-            getallen[2] = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < getallen.Length; i++)
+            {
+                getallen[i] = Convert.ToInt32(Console.ReadLine());
+            }
             int resultaat = getallen[0] + getallen[1] + getallen[2];
             Console.WriteLine("De getallen opgeteld zijn: {0} + {1} + {2} = {3}", getallen[0], getallen[1], getallen[2], resultaat);
             Console.ReadKey();
