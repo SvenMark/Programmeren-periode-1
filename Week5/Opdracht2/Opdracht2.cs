b68ab10091a3b2bec7db2fbf2baf59a004d926af
Sven Mark Hofstra <hofs1600.student.nhl.nl>
2016-10-04 14:25:57 +0200

    Opdracht2Week5Fix

--- Week5/Opdracht2/Opdracht2.cs
+++ Week5/Opdracht2/Opdracht2.cs
@@ -13,18 +13,13 @@ namespace Opdracht2
             int[] getallen;
             getallen = new int[9];
             Console.WriteLine("9 getallen aub: ");
-            getallen[0] = Convert.ToInt32(Console.ReadLine());
-            getallen[1] = Convert.ToInt32(Console.ReadLine());
-            getallen[2] = Convert.ToInt32(Console.ReadLine());
-            getallen[3] = Convert.ToInt32(Console.ReadLine());
-            getallen[4] = Convert.ToInt32(Console.ReadLine());
-            getallen[5] = Convert.ToInt32(Console.ReadLine());
-            getallen[6] = Convert.ToInt32(Console.ReadLine());
-            getallen[7] = Convert.ToInt32(Console.ReadLine());
-            getallen[8] = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < getallen.Length; i++)
+            {
+                getallen[i] = Convert.ToInt32(Console.ReadLine());
+            }
             Console.Write("Welk getal wilt u zien? ");
             int gevraagd = Convert.ToInt32(Console.ReadLine());
-            int antwoord = getallen[gevraagd];
+            int antwoord = getallen[gevraagd - 1];
             Console.WriteLine("Het getal op positie {0} is {1}", gevraagd, antwoord);
             Console.ReadKey();
         }
