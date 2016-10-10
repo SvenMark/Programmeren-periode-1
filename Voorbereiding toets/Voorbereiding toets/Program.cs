e8807ff600a79ec4d18ee00569e3b9916c3d628e
Sven Mark Hofstra <hofs1600@student.nhl.nl>
2016-10-10 16:25:43 +0200

    Voorbeeld toets opgave 1

---
+++ Voorbereiding toets/Voorbereiding toets/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+ {    // vraag1: schrijf een programma die je drie getalen invoeren en reken de gemiddelde er van
+        public static void vraag1()
+        {
+            double[] getallen = new double[3];
+            for (int i = 0; i < getallen.Length; i++)
+            {
+                Console.Write("Getal {0}: ", i + 1);
+                getallen[i] = Convert.ToDouble(Console.ReadLine());
+            }
+            Console.WriteLine(getallen.Average());            
+        }
+     // vraag2:
+     // declare een array getalen die heeft size7 
+    // vul de array met getalen 
+   //print de inhoud van de array 
+  // print de array van achter naar voeren 
+        public static void vraag2()
+        {
+             
+        }
+        // vraag3:  
+        // enter 3 getalen en print de klein getal uit.
+        public static void vraag3()
+        { 
+        }
+        static void Main(string[] args)
+        {
+            vraag1();
+            //vraag2();
+            //vraag3();
+            Console.ReadLine();
+
+        }
+    }
+}
