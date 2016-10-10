2e55b5a53a6e9edde86c11ca366475efa5615110
Sven Mark Hofstra <hofs1600@student.nhl.nl>
2016-10-10 16:38:40 +0200

    Opgave 3 dun

--- Voorbereiding toets/Voorbereiding toets/Program.cs
+++ Voorbereiding toets/Voorbereiding toets/Program.cs
@@ -40,12 +40,19 @@ namespace ConsoleApplication6
         // vraag3:  
         // enter 3 getalen en print de klein getal uit.
         public static void vraag3()
-        { 
+        {
+            int[] getallen = new int[3];
+            for (int i = 0; i < getallen.Length; i++)
+            {
+                Console.Write("Getal {0}: ", i + 1);
+                getallen[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine(getallen.Min());
         }
         static void Main(string[] args)
         {
             //vraag1();
-            vraag2();
+            //vraag2();
             //vraag3();
             Console.ReadLine();
 
