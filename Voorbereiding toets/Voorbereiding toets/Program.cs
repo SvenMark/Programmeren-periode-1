adb5c98eeb824cdff2de01fa799c4ffa6ad18445
Sven Mark Hofstra <hofs1600.student.nhl.nl>
2016-10-11 10:09:22 +0200

    blyat

--- Voorbereiding toets/Voorbereiding toets/Program.cs
+++ Voorbereiding toets/Voorbereiding toets/Program.cs
@@ -10,13 +10,17 @@ namespace ConsoleApplication6
  {    // vraag1: schrijf een programma die je drie getalen invoeren en reken de gemiddelde er van
         public static void vraag1()
         {
-            double[] getallen = new double[3];
-            for (int i = 0; i < getallen.Length; i++)
+            double getallen = 0;
+            double getal = 0;
+            double gem = 0;
+            for (int i = 0; i < 3; i++)
             {
                 Console.Write("Getal {0}: ", i + 1);
-                getallen[i] = Convert.ToDouble(Console.ReadLine());
+                getal = Convert.ToDouble(Console.ReadLine());
+                getallen += getal;
             }
-            Console.WriteLine(getallen.Average());            
+            gem = getallen / 3;
+            Console.WriteLine("Gemiddelde: {0}", gem);
         }
      // vraag2:
      // declare een array getalen die heeft size7 
@@ -31,29 +35,34 @@ namespace ConsoleApplication6
                 Console.Write("Getal {0}: ", i + 1);
                 getallen[i] = Convert.ToInt32(Console.ReadLine());
             }
-            Array.Reverse(getallen);
-            foreach (int i in getallen)
+            Console.WriteLine("Reversed: ");
+            for (int j = getallen.Length - 1; j >= 0; j--)
             {
-                Console.WriteLine(i);
+                Console.WriteLine(getallen[j]);
             }
         }
         // vraag3:  
         // enter 3 getalen en print de klein getal uit.
         public static void vraag3()
         {
-            int[] getallen = new int[3];
-            for (int i = 0; i < getallen.Length; i++)
+            Console.Write("Getal 1: ");
+            int smallest = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < 3; i++)
             {
                 Console.Write("Getal {0}: ", i + 1);
-                getallen[i] = Convert.ToInt32(Console.ReadLine());
+                int getallen = Convert.ToInt32(Console.ReadLine());
+                if (getallen < smallest)
+                {
+                    smallest = getallen;
+                }
             }
-            Console.WriteLine(getallen.Min());
+            Console.WriteLine(smallest);
         }
         static void Main(string[] args)
         {
             //vraag1();
             //vraag2();
-            //vraag3();
+            vraag3();
             Console.ReadLine();
 
         }
